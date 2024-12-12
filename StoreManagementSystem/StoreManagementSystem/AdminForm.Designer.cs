60d478b5d68005f20369830fa07c588f6545dae2
buihai041203 <buihai0412@gmail.com>
2024-12-12 19:28:41 +0700

    Database-12-12

---
+++ StoreManagementSystem/StoreManagementSystem/AdminForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace StoreManagementSystem
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnManageProducts = new System.Windows.Forms.Button();
+            this.btnManageEmployees = new System.Windows.Forms.Button();
+            this.btnManageCustomers = new System.Windows.Forms.Button();
+            this.btnTransactionHistory = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnManageProducts
+            // 
+            this.btnManageProducts.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnManageProducts.Location = new System.Drawing.Point(28, 41);
+            this.btnManageProducts.Name = "btnManageProducts";
+            this.btnManageProducts.Size = new System.Drawing.Size(228, 51);
+            this.btnManageProducts.TabIndex = 0;
+            this.btnManageProducts.Text = "Manage Products";
+            this.btnManageProducts.UseVisualStyleBackColor = false;
+            this.btnManageProducts.Click += new System.EventHandler(this.btnManageProducts_Click);
+            // 
+            // btnManageEmployees
+            // 
+            this.btnManageEmployees.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnManageEmployees.Location = new System.Drawing.Point(28, 111);
+            this.btnManageEmployees.Name = "btnManageEmployees";
+            this.btnManageEmployees.Size = new System.Drawing.Size(228, 51);
+            this.btnManageEmployees.TabIndex = 0;
+            this.btnManageEmployees.Text = "Manage Employees";
+            this.btnManageEmployees.UseVisualStyleBackColor = false;
+            this.btnManageEmployees.Click += new System.EventHandler(this.btnManageEmployees_Click);
+            // 
+            // btnManageCustomers
+            // 
+            this.btnManageCustomers.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnManageCustomers.Location = new System.Drawing.Point(28, 183);
+            this.btnManageCustomers.Name = "btnManageCustomers";
+            this.btnManageCustomers.Size = new System.Drawing.Size(228, 51);
+            this.btnManageCustomers.TabIndex = 0;
+            this.btnManageCustomers.Text = "Manage Customers";
+            this.btnManageCustomers.UseVisualStyleBackColor = false;
+            this.btnManageCustomers.Click += new System.EventHandler(this.btnManageCustomers_Click);
+            // 
+            // btnTransactionHistory
+            // 
+            this.btnTransactionHistory.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTransactionHistory.Location = new System.Drawing.Point(28, 254);
+            this.btnTransactionHistory.Name = "btnTransactionHistory";
+            this.btnTransactionHistory.Size = new System.Drawing.Size(228, 51);
+            this.btnTransactionHistory.TabIndex = 0;
+            this.btnTransactionHistory.Text = "Transaction History";
+            this.btnTransactionHistory.UseVisualStyleBackColor = false;
+            this.btnTransactionHistory.Click += new System.EventHandler(this.btnTransactionHistory_Click);
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnLogOut.Location = new System.Drawing.Point(28, 326);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(228, 51);
+            this.btnLogOut.TabIndex = 0;
+            this.btnLogOut.Text = "Log Out";
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Image = global::StoreManagementSystem.Properties.Resources.ad;
+            this.pictureBox1.Location = new System.Drawing.Point(283, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 373);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(638, 418);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnTransactionHistory);
+            this.Controls.Add(this.btnManageCustomers);
+            this.Controls.Add(this.btnManageEmployees);
+            this.Controls.Add(this.btnManageProducts);
+            this.Name = "AdminForm";
+            this.Text = "AdminForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnManageProducts;
+        private System.Windows.Forms.Button btnManageEmployees;
+        private System.Windows.Forms.Button btnManageCustomers;
+        private System.Windows.Forms.Button btnTransactionHistory;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ StoreManagementSystem/StoreManagementSystem/SaleForm.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StoreManagementSystem
+{
+    public partial class SaleForm : Form
+    {
+        private string currentView = "Product"; // Biến trạng thái hiển thị hiện tại
+
+        public SaleForm()
+        {
+            InitializeComponent();
+
+            // Maximize the form to fill the screen
+            this.WindowState = FormWindowState.Maximized;
+
+            // Center the form on the screen
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Disable resizing (Fixed size)
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+
+            // Optional: Disable the maximize button as well if you don't want the user to maximize it
+            this.MaximizeBox = false;
+        }
+
+        private void SaleForm_Load(object sender, EventArgs e)
+        {
+            currentView = "Product"; // Đặt mặc định là hiển thị dữ liệu sản phẩm
+            LoadProductData();       // Gọi phương thức để tải dữ liệu sản phẩm
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBoxSearch.Text; // Lấy nội dung tìm kiếm từ TextBox
+
+            if (string.IsNullOrEmpty(searchText))
+            {
+                if (currentView == "Product") LoadProductData();
+                else if (currentView == "History") LoadHistoryData();
+                else if (currentView == "Customer") LoadCustomerData();
+            }
+            else
+            {
+                if (currentView == "Product") SearchProduct(searchText);
+                else if (currentView == "History") SearchHistory(searchText);
+                else if (currentView == "Customer") SearchCustomer(searchText);
+            }
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            currentView = "Product";
+            LoadProductData(); // Hiển thị dữ liệu sản phẩm
+        }
+
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            currentView = "History";
+            LoadHistoryData(); // Hiển thị dữ liệu lịch sử giao dịch
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            currentView = "Customer";
+            LoadCustomerData(); // Hiển thị dữ liệu thông tin khách hàng
+        }
+
+        private void LoadProductData()
+        {
+            string query = "SELECT * FROM Product"; // Lấy tất cả dữ liệu từ bảng Product
+
+            using (SqlConnection conn = new SqlConnection(Connection.SQLConnection))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt; // Gán DataTable vào DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tải dữ liệu sản phẩm: " + ex.Message);
+                }
+            }
+        }
+
+        private void LoadHistoryData()
+        {
+            string query = @"
+        SELECT c.CustomerName, p.Name AS ProductName, ph.Quantity, ph.PurchaseDate
+        FROM PurchaseHistory ph
+        JOIN Customer c ON ph.CustomerID = c.CustomerID
+        JOIN Product p ON ph.ProductCode = p.Code";  // Lấy tên sản phẩm từ bảng Product
+
+            using (SqlConnection conn = new SqlConnection(Connection.SQLConnection))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tải dữ liệu lịch sử giao dịch: " + ex.Message);
+                }
+            }
+        }
+
+        private void LoadCustomerData()
+        {
+            string query = "SELECT  CustomerName, PhoneNumber, Email, Address FROM Customer";
+
+            using (SqlConnection conn = new SqlConnection(Connection.SQLConnection))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tải dữ liệu khách hàng: " + ex.Message);
+                }
+            }
+        }
+
+        private void SearchProduct(string searchText)
+        {
+            string query = "SELECT * FROM Product WHERE Code LIKE @searchText OR Name LIKE @searchText";
+
+            using (SqlConnection conn = new SqlConnection(Connection.SQLConnection))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@searchText", "%" + searchText + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tìm kiếm sản phẩm: " + ex.Message);
+                }
+            }
+        }
+
+        private void SearchHistory(string searchText)
+        {
+            string query = @"
+        SELECT c.CustomerName, p.Name AS ProductName, ph.Quantity, ph.PurchaseDate
+        FROM PurchaseHistory ph
+        JOIN Customer c ON ph.CustomerID = c.CustomerID
+        JOIN Product p ON ph.ProductCode = p.Code
+        WHERE c.CustomerName LIKE @searchText OR p.Name LIKE @searchText OR ph.ProductCode LIKE @searchText";
+
+            using (SqlConnection conn = new SqlConnection(Connection.SQLConnection))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@searchText", "%" + searchText + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataGridView1.DataSource = dt; // Gán DataTable vào DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tìm kiếm lịch sử giao dịch: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        private void SearchCustomer(string searchText)
+        {
+            string query = @"
+        SELECT CustomerID, CustomerName, PhoneNumber, Email, Address
+        FROM Customer
+        WHERE CustomerName LIKE @searchText OR PhoneNumber LIKE @searchText";
+
+            using (SqlConnection conn = new SqlConnection(Connection.SQLConnection))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@searchText", "%" + searchText + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataGridView1.DataSource = dt; // Gán DataTable vào DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tìm kiếm khách hàng: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginApp loginForm = new LoginApp();
+            loginForm.Show();
+        }
+    }
+}
